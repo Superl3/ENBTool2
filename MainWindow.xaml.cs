a530a696b2358be43920e15a064adca8ae05a160
Superl3 <31559637+Superl3@users.noreply.github.com>
2018-06-08 13:30:24 +0900

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ENBTool
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] enbseries = File.ReadAllLines(___Path.Text + "\\enbseries.ini");
+            string[] weather = File.ReadAllLines(___Path.Text + "\\enbseries\\_weatherlist.ini");
+            foreach(string line in weather)
+            {
+                if (line.Contains("FileName="))
+                {
+
+                }
+            }
+        }
+    }
+}
